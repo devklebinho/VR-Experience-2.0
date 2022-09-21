0f20f4e85534e97d4ab60fd7c24563bdeb1dd855
kleberkgns <kleber.silva.kgns@gmail.com>
2022-09-21 17:59:12 -0300

    Unit 02 - bullet

---
+++ Assets/CardboardUnityAdventure/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject bullet;
+    public float speed = 2f;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            GameObject createdBullet = Instantiate(bullet);
+            createdBullet.transform.position = transform.position;
+            Rigidbody bullet_rig = createdBullet.GetComponent<Rigidbody>();
+            Camera cam = GetComponentInChildren<Camera>();
+
+            bullet_rig.velocity = cam.transform.rotation * Vector3.forward * speed;
+        }
+    }
+}
